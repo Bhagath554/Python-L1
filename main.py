275b939a9fd3fb28a5e528c58246832f54992f19
Bhagath554 <bhagat.pinnaka@gmail.com>
2024-08-20 19:00:17 +0530

    firstCommit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("hello world")
+number = int(input("Enter Number to check"))
+print("Number to be Checked:", number)
+
+
+
+if number%2==0 :
+    print("This is an Even Number")
+
+else: 
+    print("This is an Odd Number")
+
