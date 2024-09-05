f4c4256a82f95e0b8d28bed0eb565969bbc754f9
Bhagath554 <bhagat.pinnaka@gmail.com>
2024-09-05 19:28:14 +0530

    Class 8 p 2

--- class 8 p2/main.py
+++ class 8 p2/main.py
@@ -7,5 +7,11 @@ print("Updated Set:",my_Set)
 set1 = (my_Set)
 set2 = ("1","2","4","4")
 
-print("\n Union of Two Sets:")
+print("\n Set 1,set1")
+print("\nSet 2,set2")
+print("Difference")
+print(set1.difference(set2))
+print("Symmetric Difference")
+print(set1.symmetric_difference(set2))
+
 
