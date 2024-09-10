2329863ba7d7b61b9a49360d1bf21aaa75552511
Bhagath554 <bhagat.pinnaka@gmail.com>
2024-09-10 19:46:00 +0530

    project

---
+++ No. Guessing game/main.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a No."))
+
+
+x = 10
+ 
+if x > 10:
+     print("You guessed it Wrong")
+elif x == 10:
+    print("Congrats you got it Right")
+else:
+     print("You guessed it Wrong")
