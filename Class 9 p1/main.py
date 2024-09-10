4e38ca8ce59e3620f724481c04ea7a457d92c83a
Bhagath554 <bhagat.pinnaka@gmail.com>
2024-09-10 18:57:17 +0530

    Class 9 p2

--- Class 9 p1/main.py
+++ Class 9 p1/main.py
@@ -1,5 +1,16 @@
-class Student:
- grade=10
- print("Hello My Name is Bhagath from Grade",grade)
 
-ob = Student()
+
+class student:
+  grade= 10
+  name= "Bhagath" 
+  def introduction(self):
+    print("Hello I am a Student")
+
+  def details(self):
+        print("My Name is",self.name)
+        print("I study in grade",self.grade)
+
+
+ob = student()
+ob. introduction()
+ob. details()
