f3c8f9c3abfb95f64f89bc6d5fb5f47e09a7f75b
sureshsk9 <suresh@settleking.com>
2024-09-29 11:51:37 +0530

    Aug 15 class

---
+++ AUG 15 CLASS/main.py
@@ -0,0 +1,24 @@
+name = "Penguine"
+age = 15
+student = True
+weight = 38.5
+
+print("Name:",name)
+print("Datatype Name is:",type(name))
+print("Age",age)
+print("Datatype of ageis:",type(age))
+print("is_student",student)
+print("Datatypeof student is:",type(student))
+
+print("Weight",weight)
+print("Datatype of weight is:",type(weight))
+
+print("\n After type casting...")
+
+age = str(age)
+print(age)
+print("Datatype of ageis:",type(age))
+
+weight = int(weight)
+print(weight)
+print("Datatype of weight is:",type(weight))
