296308877a3f28ff3d7558128f765c05ada7def9
sureshsk9 <suresh@settleking.com>
2024-09-29 12:09:10 +0530

    p-3

---
+++ AUG 15 CLASS/p-3/main.py
@@ -0,0 +1,15 @@
+firstname = "Codingal"
+lastname = "Educations"
+fname = firstname+lastname
+example = "Haa"*5
+
+print("Firstname :",firstname)
+print("lastname :",lastname)
+print("Fname :",fname)
+print("String multiplied the 5 times gives result  :",example)
+
+word = "Coding"
+print("length of the string :",len(word))
+print("First letter of the string :",word[0])
+print("Last letter of the string :",word[5])
+print("Sliced string :",word[1:3])
