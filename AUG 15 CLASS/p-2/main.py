17c3c40def73ba5b04d7550b24e4571f9d7f0638
sureshsk9 <suresh@settleking.com>
2024-09-29 12:02:22 +0530

    Aug 15 p2

---
+++ AUG 15 CLASS/p-2/main.py
@@ -0,0 +1,22 @@
+num1 = 45
+num2 = 15
+print("Number1 :",num1)
+print("Number2:",num2)
+
+print("Addition :",num1 + num2)
+print("Subtraction :",num1 - num2)
+print("Division :",num1/num2)
+print("Multiplication",num1 *num2)
+
+print("Floor Division :",num1//num2)
+print("Modulus Operator :",num1%num2)
+print("Square:",num2**2)
+print("Square Root:",num2**0.5)
+
+print("Equal ?",num1==num2)
+print("Num 1 Greater?",num1>num2)
+print("Num2 Greater ?",num1<num2)
+print(" Not Equal ?",num1!=num2)
+
+result = num1/num2+1
+print("The Answer of the given equation is:",result)
