3078390436f2348b84363b9a2171cb55f3bcc069
sureshsk9 <suresh@settleking.com>
2024-09-29 12:14:57 +0530

    p-3

---
+++ AUG 15 CLASS/p-4/main.py
@@ -0,0 +1,10 @@
+x = input("Enter the value of x")
+y = input("Enter the value of y")
+
+temp = x
+x = y
+y = temp
+
+print("Value of x after swapping",x)
+print("Value of y after swapping",y)
+print("Value of temp after swapping",temp)
