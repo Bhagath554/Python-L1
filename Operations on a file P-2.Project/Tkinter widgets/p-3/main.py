d8f9e556c8476d723c53f7b684cb2305d452f07e
sureshsk9 <suresh@settleking.com>
2024-10-10 19:12:50 +0530

    P-3

--- Operations on a file P-2.Project/Tkinter widgets/p-3/main.py
+++ Operations on a file P-2.Project/Tkinter widgets/p-3/main.py
@@ -2,3 +2,23 @@ from tkinter import *
 
 root = Tk()
 
+root.geometry('400x300')
+root.title("Main")
+
+def topwin() :
+       top = Toplevel()
+       top.geometry('180x100')
+       top.title("Toplevel")
+
+       l2 = Label(root,text="This is Toplevel Window")
+
+       l2.pack()
+
+       top.mainloop()
+
+l = Label(root,text="This is Root Window")
+btn = Button(root,text= "Click Here to Open Anathor Window",command=topwin)
+l.pack()
+btn.pack()
+
+root.mainloop()
