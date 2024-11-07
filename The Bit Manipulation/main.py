2da1dd01c92f1275ee63019b70d343b2ed213a7d
sureshsk9 <suresh@settleking.com>
2024-11-07 18:54:36 +0530

    Bit Manipulation P-1

---
+++ The Bit Manipulation/main.py
@@ -0,0 +1,14 @@
+num1 = 15
+num2 = 5
+
+print("num1 & num2 :",num1 & num2)
+
+print("\nnum1 | num2:",num1 | num2)
+
+print("\n~num2:",num2)
+
+print("\nnum1 ^ num2:",num1 ^ num2)
+
+print("\num1 << 2:",num1 <<2)
+
+print("\num2 <<1",num2 << 1)
