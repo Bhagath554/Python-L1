a1b5dcdb3f4627607f4dc5f382b37b2d40bbb0f0
sureshsk9 <suresh@settleking.com>
2024-11-10 09:13:06 +0530

    MM-2 Project

--- LCM Project/main.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter Number1 :"))
-num2 = int(input("Enter Number2 :"))
-
-while (True):
-    if (num1 % num2 == 0):
-     LCM = num1
-    break
-print("LCM of {num1} & {num2} is ",LCM)
