e45423578bbc156b8de17e7a6c524e03276e08f3
sureshsk9 <suresh@settleking.com>
2024-11-19 19:27:31 +0530

    First set bit

--- Surging with Power!!/P-2/main.py
+++ Surging with Power!!/P-2/main.py
@@ -3,11 +3,11 @@ def four(num):
 
     if (num & (~(num & (num -1)))):
 
-        while (num >1):
+        while (num >= 1):
             num >>= 1
             count += 1
 
-        if (count % 4 == 0):
+        if (count % 2 == 0):
              return True
         else :
             return False
